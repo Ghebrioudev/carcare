dbac888a794ce7ab27226f9765ef0ac360fb0dea
GHEBRIOU.KHEIREDDINE <ghebriou.kheireddine@gmail.com>
2026-07-03 23:17:22 +0100

    add vehicle modele

---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body style="background:rgb(226, 211, 211); color:rgb(143, 118, 118); font-size:40px;">
+    THIS IS MY PAGE
+
+    <h1>My Page</h1>
+    <a href="" class="btn">
+        click hete to get hacked
+    </a>
+</body>
+</html>
